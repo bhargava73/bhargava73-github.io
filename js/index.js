517acee29386083ac141cbb3589564e1bf98cd0a
root <root@Ubuntu.myguest.virtualbox.org>
2023-12-25 22:04:56 +0530

    Added dark mode switch feature.

--- js/index.js
+++ js/index.js
@@ -64,3 +64,28 @@ function scrollProgressBar() {
 }
 
 scrollProgressBar();
+
+const changeTheme = document.getElementById('toggle-theme');
+changeTheme.onchange = () => {
+  if (changeTheme.checked === true) {
+    document.documentElement.classList.remove("light");
+    document.documentElement.classList.add("dark");
+    window.localStorage.setItem('mode', 'dark');
+  } else {  
+    document.documentElement.classList.remove("dark");
+    document.documentElement.classList.add("light");
+    window.localStorage.setItem('mode', 'light');
+  }
+}
+const mode = window.localStorage.getItem('mode');
+if (mode == 'dark') {
+  changeTheme.checked = true;
+  document.documentElement.classList.remove("light");
+  document.documentElement.classList.add("dark");
+}
+
+if (mode == 'light') {
+  changeTheme.checked = false;
+  document.documentElement.classList.remove("dark");
+  document.documentElement.classList.add("light");
+}
